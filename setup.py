329be9c17dc2c710a84326857c991a90663c7bf8
nullity00 <reshmashaik3011@gmail.com>
2023-08-27 00:36:55 +0530

    setup

--- setup.py
+++ setup.py
@@ -11,26 +11,20 @@ from poly import Polynomial, Basis
 SETUP_FILE_G1_STARTPOS = 80
 SETUP_FILE_POWERS_POS = 60
 
-# G, tGx, t^2Gx, ..., t^{d-1}G
+
 @dataclass
 class Setup(object):
     #   ([1]₁, [x]₁, ..., [x^{d-1}]₁)
-    # = ( G,    xG,  ...,  x^{d-1}G ), where G is a generator of G_2
+    # = ( G,    xG,  ...,  x^{d-1}G ), where G is a generator of G_1
     powers_of_x: list[G1Point]
     # [x]₂ = xH, where H is a generator of G_2
     X2: G2Point
 
     @classmethod
     def from_file(cls, filename):
-        # reads a binary file
         contents = open(filename, "rb").read() 
-        # Byte 60 gives you the base-2 log of how many powers there are 
-        # contents[60] = log_2(d)
-        # 2 ** contents[60] = d 
-        # d = powers ( for this case, d = 2^11 )
         powers = 2 ** contents[SETUP_FILE_POWERS_POS] 
-        # ptau = G1 & G2 points, since there are 2 diff subgroups 
-        # Extract G1 points, which start at byte 80
+
         # values = integer values of g1 points
         values = [
             # bytes -> integer in little endian
@@ -42,34 +36,21 @@ class Setup(object):
                 SETUP_FILE_G1_STARTPOS, SETUP_FILE_G1_STARTPOS + 32 * powers * 2, 32
             )
         ]
-        # field modulus = prime p of a field Fp
+
         assert max(values) < b.field_modulus
         # The points are encoded in a weird encoding, where all x and y points
         # are multiplied by a factor (for montgomery optimization?). We can
         # extract the factor because we know the first point is the generator.
         factor = b.FQ(values[0]) / b.G1[0]
         values = [b.FQ(x) / factor for x in values]
+
         # range(powers) = [0, 1, 2, ..., powers - 1]
         # powers_of_x = [(values[0], values[1]), (values[2], values[3]), ...]
         # powers_of_x = [(G, x1G), (x2G, x3G), ...]
         powers_of_x = [(values[i * 2], values[i * 2 + 1]) for i in range(powers)]
         print("Extracted G1 side, X^1 point: {}".format(powers_of_x[1]))
 
-        # Search for start of G2 points. We again know that the first point is
-        # the generator.
-        # ptau = [... (60)(log_2(d)), .... (80)(G1), .... (80 + 32 * powers * 2 - 1)(x^powers * G1) , ... , G2 points ...]
         pos = SETUP_FILE_G1_STARTPOS + 32 * powers * 2
-        # G2 = (
-        #     FQ2([
-        #         10857046999023057135944570762232829481370756359578518086990519993285655852781,
-        #         11559732032986387107991004021392285783925812861821192530917403151452391805634,
-        #     ]),
-        #     FQ2([
-        #         8495653923123431417604973247489272438418190587263600148770280649306958101930,
-        #         4082367875863433681332203403145435568316851327593401208105741076214120093531,
-        #     ]),
-        # )
-        # FQ2 is a class of field points : https://github.com/ethereum/py_ecc/blob/master/py_ecc/fields/field_elements.py#L357
 
         # target = factor * 10857046999023057135944570762232829481370756359578518086990519993285655852781
         target = (factor * b.G2[0].coeffs[0]).n
@@ -94,7 +75,7 @@ class Setup(object):
         print("Extracted G2 side, X^1 point: {}".format(X2))
 
         # powers_of_x = [(G, x1G), (x2G, x3G), ...]
-        # x2 = first element of X2_values
+        # x2 = first element of X2_values (H)
         return cls(powers_of_x, X2)
 
     # Encodes the KZG commitment that evaluates to the given values in the group
